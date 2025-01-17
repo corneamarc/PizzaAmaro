7657f60841257e972daead2b5b339243dacae315
TheMarc05 <corneamarc@gmail.com>
2025-01-17 07:25:20 +0200

    Am implementat tipul de pizza personalizata si am adaugat comentarii pentru a intelege mai usor unele parti din cod

--- PizzaApp/Mancare/Client.cs
+++ PizzaApp/Mancare/Client.cs
@@ -7,31 +7,31 @@ using System.Text.Json.Serialization;
 namespace PizzaApp.Mancare; 
 
 public class Client
+{
+    [JsonIgnore]
+    public List<Order> OrderHistory { get; set; }
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+
+    public Client()
     {
-        [JsonIgnore]
-        public List<Order> OrderHistory { get; set; }
-        public string Name { get; set; }
-        public string PhoneNumber { get; set; }
-
-        public Client()
-        {
-            OrderHistory = new List<Order>();
-        }
-
-        public Client(string name, string phoneNumber) : this()
-        {
-            Name = name;
-            PhoneNumber = phoneNumber;
-        }
-
-        public string GetName() => Name;
-        public string GetPhoneNumber() => PhoneNumber;
-        public List<Order> GetOrderHistory() => OrderHistory ?? (OrderHistory = new List<Order>());
-        public void AddOrder(Order order)
-        {
-            if (OrderHistory == null)
-                OrderHistory = new List<Order>();
-            OrderHistory.Add(order);
-        }
-        public bool IsLoyalCustomer() => GetOrderHistory().Count >= 5;
+        OrderHistory = new List<Order>();
     }
+
+    public Client(string name, string phoneNumber) : this()
+    {
+        Name = name;
+        PhoneNumber = phoneNumber;
+    }
+
+    public string GetName() => Name;
+    public string GetPhoneNumber() => PhoneNumber;
+    public List<Order> GetOrderHistory() => OrderHistory ?? (OrderHistory = new List<Order>());
+    public void AddOrder(Order order)
+    {
+        if (OrderHistory == null)
+            OrderHistory = new List<Order>();
+        OrderHistory.Add(order);
+    }
+    public bool IsLoyalCustomer() => GetOrderHistory().Count >= 5;
+}
